01d249bb5dccdacc74e729508ca52e5f1bc73bdd
Matheus Ryan <matheus.ryan@discente.ufma.br>
2024-09-19 14:21:12 -0300

    Commit inicial do projeto

---
+++ app/create/index.tsx
@@ -0,0 +1,138 @@
+import { View, Text, Image, StyleSheet, Pressable, ScrollView} from 'react-native'
+import {colors} from '../../constants/colors'
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {useForm} from 'react-hook-form'
+import {Header} from '../../components/header'
+import {router} from 'expo-router'
+import {Select} from '../../components/input/select'
+import {useDataStore} from '../../store/data'
+
+
+const schema = z.object({
+    gender: z.string().min(1, {message: "O sexo é obrigatório"}),
+    objective: z.string().min(1, {message: "O objetivo é obrigatório"}),
+    level: z.string().min(1, {message: "Selecione seu nível"}),
+})
+
+type FormData = z.infer<typeof schema>
+
+
+export default function Create() {
+
+    const {control, handleSubmit, formState:{errors, isValid}} = useForm<FormData>({
+        resolver: zodResolver(schema)
+    })
+    
+
+    const setPageTwo = useDataStore(state => state.setPageTwo )
+
+    const genderOptions = [
+        { label: "Masculino", value: "masculino"},
+        { label: "Feminino", value: "feminino"}
+    ]
+
+    const levelOptions = [
+        { label: "Sedentário (pouco ou nenhum exercício durante a semana)", value: "Sedentário (pouco ou nenhum exercício durante a semana)"},
+        { label: "Levemente ativo (exercícios de 3 a 5 vezes na semana)", value: "Levemente ativo (exercicíos 1 a 3 vezes na semana)"},
+        { label: "Moderadamente ativo (exercícios de 3 a 5 vezes na semana)", value: "Moderadamente ativo (exercícios de 3 a 5 vezes na semana)" },
+        { label: "Altamente ativo (exercícios de 5 a 7 vezes na semana)", value: "Altamente ativo (exercícios de 5 a 7 vezes na semana)"}
+        
+    ]
+
+    const objectiveOptions = [
+        { label: 'Emagrecer', value: 'emagrecer' },
+        { label: 'Hipertrofia', value: 'Hipertrofia' },
+        { label: 'Hipertrofia + Definição', value: 'Hipertrofia e Definição' },
+        { label: 'Definição', value: 'Definição' },
+    ]
+
+    function handleCreate(data: FormData) {
+        setPageTwo({
+            level: data.level,
+            gender: data.gender,
+            objective: data.objective
+        })
+
+        router.push("/nutrition")
+    }
+
+ return (
+   <View style={styles.container}>
+    <Header 
+        step='Passo 2'
+        title='Finalizando dieta'
+    />
+
+    <ScrollView style = {styles.content}>
+        <Text style={styles.label}>Sexo:</Text>
+        <Select 
+        control={control}
+        name="gender"
+        placeholder="Selecione o seu sexo..."
+        error={errors.gender?.message}
+        options={genderOptions}
+        />
+
+<Text style={styles.label}>Selecione o nível de sua atividade fisíca:</Text>
+        <Select 
+        control={control}
+        name="level"
+        placeholder="Selecione o nível de sua atividade fisíca"
+        error={errors.level?.message}
+        options={levelOptions}
+        />
+
+<Text style={styles.label}>Selecione o seu objetivo:</Text>
+        <Select 
+        control={control}
+        name="objective"
+        placeholder="Selecione o seu objetivo"
+        error={errors.objective?.message}
+        options={objectiveOptions}
+        />
+
+        
+    <Pressable style = {styles.button} onPress={handleSubmit(handleCreate)}>
+        <Text style = {styles.buttonText}>Avançar</Text>
+    </Pressable>
+
+    </ScrollView>
+
+
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.background
+    },
+    label: {
+        fontSize: 16,
+        color: colors.white, 
+        fontWeight: 'bold',
+        marginBottom: 8,
+
+    },
+    content: {
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+    button: {
+        backgroundColor: colors.blue,
+        height: 44,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+
+    },
+
+    buttonText: {
+        color: colors.white,
+        fontSize: 16,
+        fontWeight: 'bold'
+
+    }
+})
